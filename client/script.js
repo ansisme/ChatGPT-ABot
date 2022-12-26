acb89767d1cba061141e7b4a4a66e7ffac7d80bd
Anshul <anshulsoni2003@gmail.com>
2022-12-26 13:23:39 +0530

    first commit

---
+++ client/script.js
@@ -0,0 +1,123 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chat_container = document.querySelector('#chat_container');
+
+let loadInterval;
+//robot loading the 3 dots and soona s the 3 dots are reached
+//it is reset to empty string and goes on
+function loader(element) {
+    element.textContent = " ";
+    loadInterval = setInterval(() => {
+        element.textContent += ".";
+
+
+        if (element.textContent === "...") {
+            element.textContent = "";
+        }
+    }, 300);
+}
+
+//typing each word letter by letter using charAt(index) and 
+//increasing the index by 1 
+function typeText(element, text) {
+    let i = 0;
+    //setInterval is the built-in function 
+    //clearInterval is used to stop the interval
+    let interval = setInterval(function() {
+        element.innerHTML += text.charAt(i);
+        i++;
+        if (i == text.length) {
+            clearInterval(interval);
+        }
+    }, 20);
+}
+
+function generateUniqueID() {
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexa = randomNumber.toString(16);
+
+    return `id-${timestamp} -${hexa}`;
+}
+
+
+//function for diffrent color and to see if the user is user or bot
+function chatStripe(isAi, value, uniqueID) {
+    return (
+        `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+          <img
+            src = ${isAi ? bot : user}
+            alt = "${isAi? 'bot':'user'}"
+          />
+        </div>
+          <div class="message" id = ${uniqueID}>${value}</div>
+        
+      </div>
+    </div>
+    `
+    )
+}
+
+
+const handleSubmit = async(e) => {
+        //prevent the default loading of the browser
+        e.preventDefault();
+        //getting data from the data inside the variable
+        const data = new FormData(form);
+
+
+        //user message
+        chat_container.innerHTML += chatStripe(false, data.get('prompt'))
+        form.reset();
+
+        //ai message
+        //uniqueID stored inside a variable and later passed into the function
+        const uniqueID = generateUniqueID();
+        chat_container.innerHTML += chatStripe(true, ' ', uniqueID);
+        chat_container.scrollTop = chat_container.scrollHeight;
+        const messageDiv = document.getElementById(uniqueID);
+        loader(messageDiv);
+
+
+
+        //FETCHING DATA FROM SERVER AS A BOT'S RESPONSE 
+        const response = await fetch('http:localhost/3000', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                prompt: data.get('prompt')
+            })
+        })
+        clearInterval(loadinterval);
+        messageDiv.innerHTML = "";
+
+        if (response.ok) {
+            const data = await response.json();
+            const parseddata = data.bot.trim();
+
+            typeText(messageDiv, parseddata);
+
+        } else {
+            const err = await response.text();
+            messageDiv.innerHTML = "Something went wrong";
+            alert(err);
+        }
+
+
+    }
+    //calling the handlesubmit function
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+    //enter keyboard key on which the handlesubmit button wuill be called
+    //and the message will be sent
+    if (e.keycode === 13) {
+        handleSubmit(e)
+    }
+})
---
+++ server/server.js
@@ -0,0 +1,52 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import { OpenAIApi, Configuration } from 'openai';
+
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAPI_KEY,
+});
+
+//instance of openai
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+app.use(cors()); //to make cross origin requests
+app.use(express.json()); //to get the json data from backend to frontend
+
+//port
+const port = 3000;
+
+app.get('/', async(req, res) => {
+    res.status(200).send({
+        message: 'Hello from Anshul',
+    })
+})
+
+app.post('/', async(req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${prompt}`, //defined in client
+            temperature: 0, //higher the temp value , highe the risks so reduce the by default temp 0.7 to 0
+            max_tokens: 3000, //max number of characters it can generate
+            top_p: 1,
+            frequency_penalty: 0.5, //not going to repeat frequent sentences often
+            presence_penalty: 0,
+
+        });
+        res.status(200).send({
+            bot: response.data.choices[0].text
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ error })
+    }
+
+})
+app.listen(`${port}`), () => {
+    console.log(`Server running on port ${port}`)
+};
